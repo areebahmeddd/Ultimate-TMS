6bd46e1084a96519057fe04a518f6cf1ac02162c
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2024-12-28 23:08:25 +0530

    feat: add task model

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High')
+    ]
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
